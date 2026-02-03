abc7302c990d4fe703a21847a0ea5e4c03190c5a
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-02-03 16:14:52 +0000

    ps1c testing

--- 6.100A/pset1/ps1c.py
+++ 6.100A/pset1/ps1c.py
@@ -1,6 +1,18 @@
 # pset1 part C
 
-annual_salary = float(input('Enter annual salary: '))
-initial_payment = int(input('Enter desired months to reach goal: '))
-total_cost = float(input('Enter total cost of dream home: '))
-monthly_salary = annual_salary / 12
+# annual_salary = float(input('Enter annual salary: '))
+initial_payment = int(input('Enter initial savings amount: '))
+total_cost = 800000 #float(input('Enter total cost of dream home: '))
+down_payment = total_cost / 4
+months = 36
+r = .00
+
+for i in range(100):
+    n = i / 100
+    r = n
+    amount_saved = initial_payment * ((1 + r / 12) ** months)
+    if amount_saved > down_payment:
+        print(r)
+        break
+
+# monthly_salary = annual_salary / 12
