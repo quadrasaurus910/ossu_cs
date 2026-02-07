a16e9e4bd552877720db6cb985996282ad7facaa
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-02-07 18:13:28 +0000

    root2 add decrementing function comment

--- root2.py
+++ root2.py
@@ -1,6 +1,9 @@
+#Figure 3.1
 #Find the cube root of a perfect cube 
+# highlight decrementing function
 x = int(input('Enter an integer: ')) 
 for ans in range(0, abs(x)+1):
+    print('Value of the decrementing function abs(x) - ans**3 is', abs(x) - ans**3)
     if ans**3 >= abs(x): 
         break
 if ans**3 != abs(x):
