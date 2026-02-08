05b8083d1274597da30cc396847476b1e78f6d6b
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-02-08 19:15:03 +0000

    epsilon

--- epsilon.py
+++ epsilon.py
@@ -1,11 +1,17 @@
+import time
+
+# figure 3.3
 x = 25
 epsilon = 0.01
 step = epsilon**2
 numGuesses = 0
 ans = 0.0
-while abs(ans**2 - x) >= epsilon and ans <= x:
+#while abs(ans**2 - x) >= epsilon and ans <= x:
+while abs(ans**2 - x) >= epsilon and ans*ans <= x:
     ans += step
     numGuesses += 1 
+    print(f"ans: {ans}, numGuesses: {numGuesses}")
+    time.sleep(1)
 print('numGuesses =', numGuesses) 
 if abs(ans**2 - x) >= epsilon:
     print('Failed on square root of', x) 
