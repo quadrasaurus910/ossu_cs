c8384ee1311a94926f3abaa7e414d35f8d046cf2
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-02-09 05:57:27 +0000

    ps1c

--- 6.100A/pset1/ps1c.py
+++ 6.100A/pset1/ps1c.py
@@ -6,18 +6,21 @@ total_cost = 800000 #float(input('Enter total cost of dream home: '))
 down_payment = total_cost / 4
 months = 36
 r = .00
-low = 0.01
+low = 0.00
 high = 1.00
 epsilon = 100
 
 
-for i in range(0, 100, 1):
-    ans = (low + high) / 2
-    r = ans
-    print(ans)
+for i in range(0, 20, 1):
+    midpoint = (low + high) / 2
+    r = round(midpoint, 2)
+    # print(r)
     amount_saved = initial_payment * ((1 + r / 12) ** months)
-    if amount_saved - down_payment >= epsilon:
-        ...
+    print(f"r: {r}, amount saved: {amount_saved}, diff: {down_payment - amount_saved}, low: {low}, high: {high}")
+    if  down_payment - amount_saved < epsilon:
+        high = midpoint
+    elif down_payment - amount_saved > epsilon:
+        low = midpoint
         #print(r)
     #print(amount_saved)
     # if amount_saved >= down_payment:
