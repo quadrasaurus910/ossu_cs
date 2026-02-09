d9a870c1ac11c3c56f7c6da35a04601d783dc1de
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-02-09 14:39:19 +0000

    ps1c

--- 6.100A/pset1/ps1c.py
+++ 6.100A/pset1/ps1c.py
@@ -5,26 +5,23 @@ initial_payment = int(input('Enter initial savings amount: '))
 total_cost = 800000 #float(input('Enter total cost of dream home: '))
 down_payment = total_cost / 4
 months = 36
+amount_saved = 0
 r = .00
-low = 0.00
-high = 1.00
+low = 0.000
+high = 1.000
 epsilon = 100
 
 
-for i in range(0, 20, 1):
+while (abs(down_payment - amount_saved)) > epsilon:
     midpoint = (low + high) / 2
-    r = round(midpoint, 2)
+    r = midpoint
     # print(r)
     amount_saved = initial_payment * ((1 + r / 12) ** months)
-    print(f"r: {r}, amount saved: {amount_saved}, diff: {down_payment - amount_saved}, low: {low}, high: {high}")
+    print(f"r: {r}, amount saved: {amount_saved}, diff: {abs(down_payment - amount_saved)}, low: {low}, high: {high}")
     if  down_payment - amount_saved < epsilon:
         high = midpoint
     elif down_payment - amount_saved > epsilon:
         low = midpoint
-        #print(r)
-    #print(amount_saved)
-    # if amount_saved >= down_payment:
-        # print(r)
-        # break
 
-# monthly_salary = annual_salary / 12
+print(f"rate: {r * 100}%")
+    
