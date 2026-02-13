21d25690f852fa9ed79b98de04679b22978be99c
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-02-13 20:22:01 +0000

    add timing to newton2

--- fingerExercises/newton2.py
+++ fingerExercises/newton2.py
@@ -1,5 +1,7 @@
 # compare  Newton-Raphson method vs bisection search
 
+import time
+
 epsilon = 0.01
 k = int(input("Enter an integer: "))
 guess = k/2.0
@@ -8,11 +10,15 @@ steps2 = 0
 low = 0.00
 high = max(1.0, k)
 ans = (low + high) / 2
+start1 = time.time()
 
 while abs(guess * guess - k) >= epsilon:
     guess = guess - (((guess ** 2) - k) / (2 * guess))
     steps += 1
+stop1 = time.time()
+elapse1 = stop1 - start1
 
+start2 = time.time()
 while abs(ans ** 2 - k) >= epsilon:
     if ans ** 2 < k:
         low = ans
@@ -20,7 +26,8 @@ while abs(ans ** 2 - k) >= epsilon:
         high = ans
     ans = (low + high) / 2.0
     steps2 += 1
-    print(ans)
+stop2 = time.time()
+elapse2 = stop2 - start2
 
-print(f"Newton-Raphson method took {steps} steps to find: {guess}.")
-print(f"Bisection search took {steps2} steps to find: {ans}.")
+print(f"Newton-Raphson method took {steps} steps to find: {guess}. Elapsed time: {elapse1}")
+print(f"Bisection search took {steps2} steps to find: {ans}. Elapsed time: {elapse2}")
