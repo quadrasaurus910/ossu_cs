cd2db732c6a563019a76add841b52a33bae7f84d
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-03-10 11:26:32 +0000

    fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,16 @@
+# figure 4.7
+# Recursive implementation of Fibonacci sequence
+
+def fib(n):
+    """Assumes n int >= 0
+    Returns Fibonacci of n""" 
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+def testFib(n):
+    for i in range(n+1):
+        print('fib of', i, '=', fib(i))
+
+print(testFib(20))
