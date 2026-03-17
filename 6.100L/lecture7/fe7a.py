7fec16151d4f575e76eb457714e421b35b221adb
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-03-17 22:50:13 +0000

    add lecture7 finger exercises

---
+++ 6.100L/lecture7/fe7a.py
@@ -0,0 +1,10 @@
+def eval_quadratic(a, b, c, x):
+    """
+    a, b, c: numerical values for the coefficients of a quadratic equation
+    x: numerical value at which to evaluate the quadratic.
+    Returns the value of the quadratic a×x² + b×x + c.
+    """
+    # Your code here
+
+# Examples:    
+print(eval_quadratic(1, 1, 1, 1)) # prints 3
