5fa412e52d10e02466f061dc6fad6a03c3bc8e6e
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-03-25 22:23:57 +0000

    fe9a

---
+++ 6.100L/lecture9/fe9a.py
@@ -0,0 +1,15 @@
+def dot_product(tA, tB):
+    """
+    tA: a tuple of numbers
+    tB: a tuple of numbers of the same length as tA
+    Assumes tA and tB are the same length.
+    Returns a tuple where the:
+    * first element is the length of one of the tuples
+    * second element is the sum of the pairwise products of tA and tB
+    """
+    # Your code here
+
+# Examples:
+tA = (1, 2, 3)
+tB = (4, 5, 6)   
+print(dot_product(tA, tB)) # prints (3,32)
