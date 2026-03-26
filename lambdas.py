3e6538b2d590cf516aaaeac77d9c8c9876855b70
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-03-26 11:48:28 +0000

    lambdas

--- lambdas.py
+++ lambdas.py
@@ -1,2 +1,8 @@
 for i in map(lambda x,y : x*y, [1,2,3,4], [2,3,4,5]):
-    print(i)
+    ...
+    #print(i)
+
+string = 'word'
+cubed = lambda x : x**3
+print([x**3 for x in range(1,11)])
+print([cubed(x) for x in range(1,11)] )
