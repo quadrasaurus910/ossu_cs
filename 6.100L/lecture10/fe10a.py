7c2f78468924ec753019753456d5bad19ca301a0
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-04-03 01:27:53 +0000

    fe10a

--- 6.100L/lecture10/fe10a.py
+++ 6.100L/lecture10/fe10a.py
@@ -5,8 +5,13 @@ def all_true(n, Lf):
     with n as a parameter. Otherwise returns False. 
     """
     # Using list comprehension to iterate over list executing function
-    print([f(n) for f in Lf])
+
+    if False in [f(n) for f in Lf]:
+        return False
+    else:
+        return True
 
 # Examples:
+squared = lambda x : x**2 % 2 == 0 
 cubed = lambda x : x**3 % 2 == 0    
-all_true(7, [cubed]) # prints 6
+print(all_true(6, [squared, cubed])) # prints 6
