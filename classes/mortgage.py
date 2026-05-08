610df720eb15573641b7d018f8c8fb948cb7ff78
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-05-08 11:32:42 +0000

    mortgage

--- classes/mortgage.py
+++ classes/mortgage.py
@@ -60,3 +60,18 @@ class TwoRate(Mortgage):
                                        self.rate, 
                                        self.months - self.teaserMonths)
         Mortgage.makePayment(self)
+
+def compareMortgages(amt, years, fixedRate, pts, ptsRate, varRate1, varRate2, varMonths):
+    totMonths = years*12
+    fixed1 = Fixed(amt, fixedRate, totMonths)
+    fixed2 = FixedWithPts(amt, ptsRate, totMonths, pts)
+    twoRate = TwoRate(amt, varRate2, totMonths, varRate1, varMonths) 
+    morts = [fixed1, fixed2, twoRate]
+    for m in range(totMonths):
+        for mort in morts: 
+            mort.makePayment()
+    for m in morts: 
+        print(m)
+        print(' Total payments = $' + str(int(m.getTotalPaid())))
+
+compareMortgages(amt=200000, years=30, fixedRate=0.07,pts = 3.25, ptsRate=0.05, varRate1=0.045,varRate2=0.095, varMonths=48)
