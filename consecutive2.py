64d79554da7bbbd8252571c91db8c9c8e4a1d4db
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-05-29 18:42:37 +0000

    consecutive2

--- consecutive2.py
+++ consecutive2.py
@@ -4,15 +4,20 @@ import sys
 def main():
     if sys.argv != 4:
         ints = []
-        for i in sys.argv:
+        for i in sys.argv[1:]:
             try:
                 ints.append(int(i))
+                print(i)
             except:
                 sys.exit("Arguments must be integers")
-        consecutiveInts(ints[:])
+        print(ints)
+        print(consecutiveInts(ints[0], ints[1], ints[2]))
 
 
 def consecutiveInts(s, n, offset=1):
-    ...
+    print((s - offset*n)/n)
+    initial = (s  - (offset * n)) / n
+    return [i for i in range(n)]
 
-consecutiveInts(1,2)
+if __name__ == "__main__":
+    main()
