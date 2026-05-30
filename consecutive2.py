1731190f531b05ee489b20e28781786c5a140f04
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-05-30 22:04:31 +0000

    consecutive2

--- consecutive2.py
+++ consecutive2.py
@@ -3,6 +3,7 @@ import sys
 
 def main():
     arglen = len(sys.argv)
+    print(sys.argv)
     if arglen > 1:
         ints = []
         for i in sys.argv[1:]:
@@ -14,12 +15,17 @@ def main():
             ints.append(int(i))
         if arglen == 4:
             print(consecutiveInts(ints[0], ints[1], ints[2]))
-        if arglen == 3:
+        elif arglen == 3:
             print(consecutiveInts(ints[0], ints[1]))
+        elif arglen == 2:
+            na = input("Please enter amount of consecutive integers: ")
+            print(consecutiveInts(ints[0], int(na)))
+
 
 
 
 def consecutiveInts(s, n, offset=1):
+    print(f"s: {s}, n: {n}, offset: {offset}")
     print((s - offset*n)/n)
     initial = (s  - (offset * n)) / n
     return [i for i in range(n)]
