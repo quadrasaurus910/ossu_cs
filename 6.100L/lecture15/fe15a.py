4d9f4bc186c95e037eaf6578a273894177049482
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-06-14 22:37:11 +0000

    lecture 15 finger exercise

---
+++ 6.100L/lecture15/fe15a.py
@@ -0,0 +1,16 @@
+def recur_power(base, exp):
+    """
+    base: int or float.
+    exp: int >= 0
+
+    Returns base to the power of exp using recursion.
+    Hint: Base case is when exp = 0. Otherwise, in the recursive
+    case you return base * base^(exp-1).
+    """
+    # Your code here  
+    if exp == 0:
+        return 1
+    return base * base ** (exp - 1) #recur_power(base, exp - 1)
+
+# Examples:
+print(recur_power(2,5))  # prints 32
