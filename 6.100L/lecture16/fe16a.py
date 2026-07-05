c54e408dd162f9e18701a8b523ccaa83ebc3f550
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-07-05 15:57:55 +0000

    lecture 16 finger exercise

---
+++ 6.100L/lecture16/fe16a.py
@@ -0,0 +1,10 @@
+def flatten(L):
+    """ 
+    L: a list 
+    Returns a copy of L, which is a flattened version of L 
+    """
+    # Your code here  
+
+# Examples:
+L = [[1,4,[6],2],[[[3]],2],4,5]
+print(flatten(L)) # prints the list [1,4,6,2,3,2,4,5]
