a264106387b60e7e4f341fa8e67135321a9009dc
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-07-05 13:34:45 +0000

    lecture 15 finger exercise

--- 6.100L/lecture15/fe15a.py
+++ 6.100L/lecture15/fe15a.py
@@ -8,9 +8,10 @@ def recur_power(base, exp):
     case you return base * base^(exp-1).
     """
     # Your code here  
-    if exp == 0:
+    if exp <= 0:
         return 1
-    return base * base ** (exp - 1) #recur_power(base, exp - 1)
+    #return base * base ** (exp - 1) #recur_power(base, exp - 1)
+    return base * recur_power(base, exp - 1)
 
 # Examples:
 print(recur_power(2,5))  # prints 32
