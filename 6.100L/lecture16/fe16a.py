1240e67c5f4df91910916200b04f5d19ad5efc9b
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-07-06 16:21:19 +0000

    lecture 16 finger exercise

--- 6.100L/lecture16/fe16a.py
+++ 6.100L/lecture16/fe16a.py
@@ -4,11 +4,13 @@ def flatten(L):
     Returns a copy of L, which is a flattened version of L 
     """
     # Your code here
-    if len(L) == 1:
-        if type(L[0]) == list:
-            ...
+    if type(L) != list:
+        return L
+    else:
+        if len(L) == 1:
+            return L[0]
         else:
-            return L[0]  
+            return [flatten(L[0])] + [flatten(L[1:])]
 
 # Examples:
 L = [[1,4,[6],2],[[[3]],2],4,5]
