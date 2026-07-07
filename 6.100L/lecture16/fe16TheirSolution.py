7adedf1207e22d5cea6932eae99273d68aace87d
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-07-07 23:47:00 +0000

    lecture 16 finger exercise, their solution

---
+++ 6.100L/lecture16/fe16TheirSolution.py
@@ -0,0 +1,17 @@
+def flatten(L):
+    """ 
+    L: a list 
+    Returns a copy of L, which is a flattened version of L 
+    """
+    # Your code here
+    result = []
+    for i in L:
+        if type(i) == list:
+            result.extend(flatten(i))
+        else:
+            result.append(i)
+    return result
+
+# Examples:
+L = [[1,4,[6],2],[[[3]],2],4,5]
+print(flatten(L)) # prints the list [1,4,6,2,3,2,4,5]
