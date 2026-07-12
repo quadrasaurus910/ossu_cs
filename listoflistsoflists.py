42c754fca83ee394b28ae1c97ef006de9c3b2bc8
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-07-12 19:17:28 +0000

    listoflistsoflists

--- listoflistsoflists.py
+++ listoflistsoflists.py
@@ -1,16 +1,19 @@
 def deep_copy(L):
     """L is a list that contains lists of lists etc."""
     print(L)
-    if len(L) == 1:
+    if len(L) == 0:
+        return []
+    elif len(L) == 1:
         if type(L[0]) == list:
-            return deep_copy(L[0])
+            return [deep_copy(L[0])]
         else:
-            return L[0]
+            print(L[0])
+            return [L[0]]
     else:
         if type(L[0]) == list:
-            return [deep_copy(L[0])], deep_copy(L[1:])
+            return [deep_copy(L[0])] + deep_copy(L[1:])
         else:
-            return [L[0], deep_copy(L[1:])]
+            return [L[0]] + deep_copy(L[1:])
 
 
 myL = ['abc', ['d'], ['e', ['f', 'g']]]
