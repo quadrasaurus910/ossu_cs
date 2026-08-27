d47cc1332894b459d61f6abcc8d808641ac5fea1
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-08-27 00:21:28 +0000

    lecture18 fe

--- 6.100L/lecture18/fe1.py
+++ 6.100L/lecture18/fe1.py
@@ -1,11 +1,11 @@
 class Circle():
     def __init__(self, radius):
         """ Initializes self with radius """
-        # your code here
+        self.radius = radius
 
     def get_radius(self):
         """ Returns the radius of self """
-        # your code here
+        return self.radius
 
     def __add__(self, c):
         """ c is a Circle object 
@@ -16,3 +16,7 @@ class Circle():
     def __str__(self):
         """ A Circle's string representation is the radius """
         # your code here
+
+
+c1 = Circle(5)
+print(c1.get_radius())
