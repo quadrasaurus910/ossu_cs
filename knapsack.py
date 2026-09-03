9a01984f8b3a29f561a0c0c294fe6262d918252e
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-09-03 13:25:06 +0000

    knapsack

--- knapsack.py
+++ knapsack.py
@@ -22,3 +22,42 @@ def weightInverse(item):
 
 def density(item):
     return item.getValue()/item.getWeight()
+
+def greedy(items, maxWeight, keyFunction): 
+    """Assumes Items a list, maxWeight >= 0,
+    keyFunction maps elements of Items to numbers""" 
+    itemsCopy = sorted(items, key=keyFunction, reverse = True) 
+    result = []
+    totalValue, totalWeight = 0.0, 0.0
+    for i in range(len(itemsCopy)):
+        if (totalWeight + itemsCopy[i].getWeight()) <= maxWeight: 
+            result.append(itemsCopy[i])
+            totalWeight += itemsCopy[i].getWeight()
+            totalValue += itemsCopy[i].getValue()
+    return (result, totalValue)
+
+def buildItems():
+    names = ['clock','painting','radio','vase','book','computer'] 
+    values = [175,90,20,50,10,200]
+    weights = [10,9,4,2,1,20]
+    Items = []
+    for i in range(len(values)):
+        Items.append(Item(names[i], values[i], weights[i])) 
+    return Items
+
+def testGreedy(items, maxWeight, keyFunction):
+    taken, val = greedy(items, maxWeight, keyFunction) 
+    print('Total value of items taken is', val)
+    for item in taken:
+        print(' ', item)
+
+def testGreedys(maxWeight = 20):
+    items = buildItems()
+    print('Use greedy by value to fill knapsack of size', maxWeight) 
+    testGreedy(items, maxWeight, value)
+    print('\nUse greedy by weight to fill knapsack of size', maxWeight)
+    testGreedy(items, maxWeight, weightInverse)
+    print('\nUse greedy by density to fill knapsack of size', maxWeight)
+    testGreedy(items, maxWeight, density)
+
+testGreedys() 
