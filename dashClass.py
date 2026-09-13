87b9e95447c47ad914a382b36b03b3b9e44dfbb5
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-09-13 14:19:01 +0000

    daqshClass

--- dashClass.py
+++ dashClass.py
@@ -4,12 +4,12 @@ class dashAst(object):
     nextId = 0
     def __init__(self, name=None):
         self.name = name
-        self.id = nextId
-        nextId += 1
+        self.id = dashAst.nextId
+        dashAst.nextId += 1
 
-    def str(self):
+    def __str__(self):
         now = datetime.now()
-        return now
+        return str(now)
 
 d1 = dashAst("beacon assistant")
 print(d1)
