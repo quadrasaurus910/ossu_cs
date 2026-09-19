3521c8b6dc628671eecaae35c027af33f1a5b351
quadrasaurus910 <146976585+quadrasaurus910@users.noreply.github.com>
2026-09-19 03:22:11 +0000

    dashClass test

--- dashClass/test.py
+++ dashClass/test.py
@@ -1,12 +1,22 @@
 from pathlib import Path
 from datetime import datetime
 
-tf = Path("data/timestamp.txt")
+tf = Path("data/timestamp.csv")
+ts = None
 
 if tf.is_file():
-    with open("data/timestamp.txt", "r") as f:
+    with open("data/timestamp.csv", "r") as f:
         ts = f.read()
+        print(ts)
+        if ts != None:
+            try:
+                print(datetime.now() - ts)
+            except:
+                print("error")
 else:
-    with open('data/timestamp.txt', "w") as f:
-        now = datetime.now()
-        f.write(now.strftime("%Y-%m-%d %H:%M:%S"))
+    with open('data/timestamp.csv', "w") as f:
+        now = datetime.now().isoformat()
+        f.write(now)
+        #f.write(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+# print(datetime.now().isoformat())
